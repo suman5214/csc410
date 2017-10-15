bfe0c7ddf2e96399313e920d52043b2a83267716
Tyler Pham <phamtyler94@gmail.com>
2017-10-15 14:44:57 -0400

    cleaned up code

--- a2/grouping_A.py
+++ a2/grouping_A.py
@@ -2,32 +2,21 @@ import sys
 # used to pretty print the matrix
 import numpy as np
 
+# process line of student pair into an array
 def process_line(array):
-    # formatted_array = []
-    # for char in array.strip().split(' '):
-    #     formatted_array.append(char)
-    # return formatted_array
-
     return array.strip().split(' ')
 
-# def declare_variables(array):
-#     for student1 in range(0, len(array)):
-#         for student2 in range(0, len(array[student1])):
-#             print('declare-const s{}_s{}\n').format(student1 + 1, array[student1][student2])
-        
+# declares all possible variables of student pairs
 def declare_variables(array):
-        
     for student1 in range(0, len(array)):
         for student2 in range(0, len(array)):
             if(student1 != student2):
                 print('declare-const s{}_s{}\n').format(student1 + 1, student2 + 1)
         
-        
 
 # ensures the correct amount of arguments
 if (len(sys.argv) > 2 or len(sys.argv) == 1):
     sys.exit('Usage: python grouping_A.py <sample-input-file-name>')
-
 # Input file
 input = open(sys.argv[1], "r")
 
