32a8cdd25542e6c23b5dd1b43fbd38f4a9909632
Tyler Pham <phamtyler94@gmail.com>
2017-10-15 14:58:07 -0400

    added domains

--- a2/grouping_A.py
+++ a2/grouping_A.py
@@ -8,10 +8,19 @@ def process_line(array):
 
 # declares all possible variables of student pairs
 def declare_variables(array):
+    all_pairs = []
     for student1 in range(0, len(array)):
         for student2 in range(0, len(array)):
             if(student1 != student2):
-                print('declare-const s{}_s{}\n').format(student1 + 1, student2 + 1)
+                formatted_line = 'declare-const s{}_s{}\n'.format(student1 + 1, student2 + 1)
+                all_pairs.append(formatted_line)
+    return all_pairs
+
+def is_priority(student1, student2, student_prefs):
+    if student2 in student_prefs[student1]:
+        return True
+    else:
+        return False
         
 
 # ensures the correct amount of arguments
@@ -22,14 +31,18 @@ input = open(sys.argv[1], "r")
 
 # an 2d array of [student, student partnership preferences]
 grouping_pref = []
+# all possible pairing boolean variables in the domain.
+all_pairs = []
 
 with open(sys.argv[1]) as file:
     for line in file:
         grouping_pref.append(process_line(line))
+all_pairs = declare_variables(grouping_pref)
 
 
 print(grouping_pref)
-declare_variables(grouping_pref)
+print(all_pairs)
+
 
 
 
