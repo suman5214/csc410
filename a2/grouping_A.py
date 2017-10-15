41e524bfef91a4232734b5b56afb569b475da0d7
Jafar <jafar.hh@hotmail.com>
2017-10-15 18:41:05 -0400

    Added functions to generate constraints

--- a2/grouping_A.py
+++ a2/grouping_A.py
@@ -1,10 +1,21 @@
 import sys
 # used to pretty print the matrix
-
+import itertools
+import math
 
 # process line of student pair into an array
 def process_line(array):
     return array.strip().split(' ')
+
+# declares all possible variables of student pairs
+def get_variables(array):
+    all_pairs = []
+    for student1 in range(0, len(array)):
+        for student2 in range(0, len(array)):
+            if(student1 != student2):
+                all_pairs.append('s{}_s{}'.format(student1 + 1, student2 + 1))
+    return all_pairs
+
 
 # declares all possible variables of student pairs
 def declare_variables(array):
@@ -12,9 +23,10 @@ def declare_variables(array):
     for student1 in range(0, len(array)):
         for student2 in range(0, len(array)):
             if(student1 != student2):
-                formatted_line = 'declare-const s{}_s{}\n'.format(student1 + 1, student2 + 1)
+                formatted_line = '(declare-const s{}_s{})\n'.format(student1 + 1, student2 + 1)
                 all_pairs.append(formatted_line)
     return all_pairs
+
 
 def is_priority(student1, student2, student_prefs):
     if student2 in student_prefs[student1]:
@@ -24,15 +36,32 @@ def is_priority(student1, student2, student_prefs):
 
 
 def build(op, arg1, arg2):
-	return "(" + op + " " + arg1 + " " + arg2 + ")"
+    return "(" + op + " " + arg1 + " " + arg2 + ")"
+
+
+def generate_constraint(op, true_args, all_args):
+    brackets = 0;
+    answer = ""
+    for i in all_args[:len(all_args) - 1]:
+        if (i in true_args):
+            answer += " (" + op + " " + i
+        else:
+            answer += " (" + op + " (not " + i + ")"
+        brackets += 1
+    if (all_args[-1] in true_args):
+        answer += " " + all_args[-1]
+    else:
+        answer += " (not " + all_args[-1] + ")"
+    answer += (")" * brackets)
+    return answer
 
 
 def s_assert(arg, weight=1, id=None):
-	if (id == None):
-		return "(assert-soft " + " " + arg + " :weight " + str(weight) + ")"
-	else:
-		return "(assert-soft " + " " + arg + " :weight " + str(weight) + ":id " + str(id) + ")"
-        
+    if (id == None):
+        return "(assert-soft " + arg + " :weight " + str(weight) + ")"
+    else:
+        return "(assert-soft " + arg + " :weight " + str(weight) + " :id " + str(id) + ")"
+
 
 # ensures the correct amount of arguments
 if (len(sys.argv) > 2 or len(sys.argv) == 1):
@@ -44,11 +73,27 @@ input = open(sys.argv[1], "r")
 grouping_pref = []
 # all possible pairing boolean variables in the domain.
 all_pairs = []
+student_count = 0
 
 with open(sys.argv[1]) as file:
     for line in file:
         grouping_pref.append(process_line(line))
-all_pairs = declare_variables(grouping_pref)
+        student_count += 1
+all_pairs = get_variables(grouping_pref)
+
+declarations = declare_variables(grouping_pref)
+for k in declarations:
+    print k
+    
+constraints = []
+print "//////////"
+for i in itertools.combinations(all_pairs, r=int(math.floor(student_count/2))):
+    constraints.append(generate_constraint("and", i, all_pairs))
+    print generate_constraint("and", i, all_pairs)
+    print "\n" + str(i)
+
+assertion = s_assert(generate_constraint("or", constraints, constraints))
+print assertion
 
 
 print(grouping_pref)
