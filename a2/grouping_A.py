fe9ac32d225ce1cb18d2848a44b47d2d0106576c
Jafar <jafar.hh@hotmail.com>
2017-10-15 20:30:00 -0400

    Writing to file for formular now working.

--- a2/grouping_A.py
+++ a2/grouping_A.py
@@ -26,7 +26,7 @@ def declare_variables(array):
     for student1 in range(0, len(array)):
         for student2 in range(0, len(array)):
             if(student1 != student2):
-                formatted_line = '(declare-const s{}_s{})\n'.format(student1 + 1, student2 + 1)
+                formatted_line = '(declare-const s{}_s{} Bool)\n'.format(student1 + 1, student2 + 1)
                 all_pairs.append(formatted_line)
     return all_pairs
 
@@ -57,6 +57,24 @@ def generate_constraint(op, true_args, all_args, weight=1):
         answer += " (not " + all_args[-1] + ")"
     answer += (")" * brackets) + " :weight " + str(weight) + ")"
     return answer
+
+
+def generate_constraint2(op, true_args, all_args, weight=1):
+    brackets = 0;
+    answer = ""
+    for i in all_args[:len(all_args) - 1]:
+        if (i in true_args):
+            answer += " (" + op + " " + i
+        else:
+            answer += " (" + op + " (not " + i + ")"
+        brackets += 1
+    if (all_args[-1] in true_args):
+        answer += " " + all_args[-1]
+    else:
+        answer += " (not " + all_args[-1] + ")"
+    answer += (")" * brackets)
+    return answer
+
 
 
 def s_assert(arg, weight=1, id=None):
@@ -97,16 +115,24 @@ with open(sys.argv[1]) as file:
     for line in file:
         grouping_pref.append(process_line(line))
         student_count += 1
+
+f = open('groupinga-formula.smt2', 'w')
+
 all_pairs = get_variables(grouping_pref)
 
 declarations = declare_variables(grouping_pref)
 for k in declarations:
-    print k
+    f.write(k)
     
 constraints = []
 for i in itertools.combinations(all_pairs, r=int(math.floor(student_count/2))):
     weight = 0
     for pair in i:
         weight += get_priority(pair, grouping_pref)
-    constraints.append(generate_constraint("and", i, all_pairs, weight))
-    print generate_constraint("and", i, all_pairs, weight)
+    constraints.append(generate_constraint2("and", i, all_pairs, weight))
+    f.write(generate_constraint("and", i, all_pairs, weight) + "\n")
+    
+
+f.write("(check-sat)\n(get-model)\n");
+
+f.close()
